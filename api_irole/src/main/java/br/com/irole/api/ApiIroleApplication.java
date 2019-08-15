20bde683961d623b6cd7a5642c9bab679b4e8f3a
unicesumar <wendel.santos@unicesumar.edu.br>
2019-08-14 22:17:40 -0300

    api hello world

---
+++ api_irole/src/main/java/br/com/irole/api/ApiIroleApplication.java
@@ -0,0 +1,13 @@
+package br.com.irole.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiIroleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiIroleApplication.class, args);
+	}
+
+}
