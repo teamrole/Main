76565cc4e888ec6477cec5256d2d8e53903c17cc
Daniel Junior <danjun48@gmail.com>
2019-09-06 22:09:42 -0300

    Filtro CORS adicionado

---
+++ api/src/main/java/br/com/irole/api/cor/CorsFilter.java
@@ -0,0 +1,45 @@
+package br.com.irole.api.cor;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class CorsFilter implements Filter{
+
+	private String origemPermitida = "http://localhost"; // TODO: Configurar para diferentes ambientes
+	
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		
+		res.setHeader("Access-Control-Allow-Origin", origemPermitida);
+		res.setHeader("Access-Control-Allow-Credentials", "true");
+		
+		if("OPTIONS".equals(req.getMethod()) && origemPermitida.equals(req.getHeader("Origin"))) {
+			res.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT, OPTIONS");
+			res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type, Accept");
+			res.setHeader("Access-Control-Max-Age", "3600");
+			
+			res.setStatus(HttpServletResponse.SC_OK);
+		}else {
+			chain.doFilter(request, response);
+		}
+		
+	}
+
+}
