96435c358e879734648490e74a532a215b14ee2b
daniel junior <danjun48@gmail.com>
2019-09-24 17:29:25 -0300

    Listar pedidos

--- api/src/main/java/br/com/irole/api/resource/PedidoController.java
+++ api/src/main/java/br/com/irole/api/resource/PedidoController.java
@@ -1,6 +1,7 @@
 package br.com.irole.api.resource;
 
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -8,6 +9,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.irole.api.event.RecursoCriadoEvent;
 import br.com.irole.api.model.Pedido;
 import br.com.irole.api.model.Sala;
+import br.com.irole.api.model.Usuario;
+import br.com.irole.api.repository.PedidoRepository;
+import br.com.irole.api.repository.SalaRepository;
 import br.com.irole.api.service.PedidoService;
 
 @RestController
@@ -26,7 +32,21 @@ public class PedidoController {
 	private PedidoService pedidoService;
 	
 	@Autowired
+	private SalaRepository SalaRepository;
+	
+	@Autowired
 	private ApplicationEventPublisher publisher;
+	
+	@GetMapping("salas/{id}")
+	public ResponseEntity<List<Pedido>> listarPedido(@PathVariable Long id){
+		Optional<Sala> salaOP = SalaRepository.findById(id);
+		if(!salaOP.isPresent())
+			return ResponseEntity.notFound().build();
+		
+		List<Pedido> pedidos = salaOP.get().getPedido();
+		
+		return !pedidos.isEmpty() ? ResponseEntity.ok(pedidos) : ResponseEntity.noContent().build();
+	}
 	
 	@PostMapping
 	public ResponseEntity<List<Pedido>> cadastrar(@RequestBody Sala sala, HttpServletResponse response){	
