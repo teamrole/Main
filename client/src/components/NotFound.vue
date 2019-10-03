23043a51ec4589e6652ea3851aae1783f003129b
Vitor <vitim_cristovao@hotmail.com>
2019-10-03 19:57:07 -0300

    Tela 404 Finalizada

--- client/src/components/NotFound.vue
+++ client/src/components/NotFound.vue
@@ -1,8 +1,7 @@
 <template>
-  <v-content style="text-align: center">
-    <h1>Erro 404</h1>
+  <v-content style="text-align: center; vertical-al">
+    <h1 style="margin-top: 75px">Erro 404</h1>
     <h2>Rota não encontrada!</h2>
-    <v-card></v-card>
     <router-link to="/Home">
       <v-btn color="white" flat value="feed">
         <span>Voltar para a pagina inicial</span>
