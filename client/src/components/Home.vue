530ecbc3460c412d51e64beb9fb002dd400f853b
Vitor <vitim_cristovao@hotmail.com>
2019-10-17 20:53:43 -0300

    Tela Home preparada para integracao com backend

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -4,7 +4,7 @@
       <v-list-item
         three-line
         class="c-nopadding"
-        @click="dialogSala = true; dialogType = 'Novo Role'"
+        @click="dialogSala = true; dialogType = 'Novo Role'; classePopup=''"
       >
         <v-card>
           <v-img class="white--text" height="30vh" src="@/assets/novo_role.png">
@@ -15,7 +15,7 @@
       <v-list-item
         three-line
         class="c-nopadding"
-        @click="dialogSala = true; dialogType = 'Entrar em um Role'"
+        @click="dialogSala = true; dialogType = 'Entrar em um Role'; classePopup='c-login-cod'"
       >
         <v-card>
           <v-img class="white--text" height="30vh" src="@/assets/entrar_role.png">
@@ -46,37 +46,119 @@
                   :label="dialogType.includes('Novo Role')? 'Nome do role':'Código do role' "
                   required
                   v-model="inputDialog"
+                  @keyup="verificaCampoKeyUp()"
+                  :error-messages="Erros.fieldMsg"
+                  :class="classePopup"
                 ></v-text-field>
               </v-col>
             </v-row>
           </v-container>
         </v-card-text>
         <v-card-actions>
-          <v-btn color="red darken1" text @click="dialogSala = false">Cancelar</v-btn>
+          <v-btn color="red darken1" text @click="dialogSala = false;inputDialog='';Erros.fieldMsg=''">Cancelar</v-btn>
           <div class="flex-grow-1"></div>
-          <v-btn color="green darken1" text @click="teste()">Confirmar</v-btn>
+          <v-btn color="green darken1" text @click="confirmaSala()">Confirmar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
+
+    <v-dialog v-model="dialogErro" persistent max-width="290">
+      <v-card>
+        <v-card-title>Erro!</v-card-title>
+        <v-card-text>
+          <span>{{alertaErroMsg}}</span>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="green darken1" text @click="dialogErro = false">Ok</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+
   </v-content>
 </template>
 
 <script>
 export default {
   methods: {
-    teste() {
+    alertaErro(msg){
+      this.alertaErroMsg = msg;
+      this.dialogErro = true;
+    },
+    verificaCampoKeyUp() {
+
       if (this.dialogType.includes("Novo Role")) {
-        console.log("Entrar na sala");
+        if (!(this.inputDialog.length < 4 || this.inputDialog.length > 20)) {
+          this.Erros.fieldMsg = "";
+        }
       } else if (this.dialogType.includes("Entrar em um Role")) {
-        console.log("Criar sala");
+        this.inputDialog = this.inputDialog.replace(/ /g, '')
+        if (this.inputDialog.length > 4) {
+          this.inputDialog = this.inputDialog.substr(0, 4);
+          this.Erros.fieldMsg = "";
+        }
+      }
+    },
+    confirmaSala() {
+      if (this.dialogType.includes("Novo Role")) {
+        if (this.inputDialog.length < 4 || this.inputDialog.length > 20) {
+          this.Erros.fieldMsg = "O nome do role deve possuir entre 4 e 20 letras";
+          return;
+        }
+        this.Erros.fieldMsg = "";
+        
+        let reqJSON = {
+          usuario: JSON.parse(localStorage.getItem("USER")),
+          nomeDoRole: this.inputDialog
+        }
+
+        console.log("REQUISICAO BACK PARA CRIAR A SALA");
+        console.log("REQJSON: " + JSON.stringify(reqJSON));
+
+        //IF RETORNO É OK, REDIRECIONA PARA A TELA DE ROLE
+
+        if(this.inputDialog == "ROLE"){
+          this.$router.push('Lobby')
+        }else{
+          this.alertaErro("Erro Ao criar o Role");
+        }
+
+      } else if (this.dialogType.includes("Entrar em um Role")) {
+        
+        if (this.inputDialog.length != 4) {
+          this.Erros.fieldMsg = "O codigo do role deve possuir 4 digitos";
+          return;
+        }
+        this.Erros.fieldMsg = "";
+
+        let reqJSON = {
+          usuario: JSON.parse(localStorage.getItem("USER")),
+          codigoDoRole: this.inputDialog
+        }
+        
+        console.log("REQUISICAO BACK PARA ENTRAR NA SALA");
+        console.log("REQJSON: " + JSON.stringify(reqJSON));
+
+        //IF RETORNO É OK, REDIRECIONA PARA A TELA DE ROLE
+        if(this.inputDialog == "6669"){
+          this.$router.push('Lobby')
+        }else{
+          this.Erros.fieldMsg = "O codigo do role é invalido";
+        }
       }
     }
   },
   data() {
     return {
       dialogSala: false,
+      dialogErro: false,
       inputDialog: "",
-      dialogType: ""
+      dialogType: "",
+      alertaErroMsg: "Erro",
+      classePopup: "",
+      Erros:{
+        fieldMsg:""
+      }
     };
   }
 };
