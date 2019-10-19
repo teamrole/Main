2fab893d87ce3e63881266ff380fbace72a2f7f8
Vitor <vitim_cristovao@hotmail.com>
2019-10-18 22:22:22 -0300

    preparando para integracao com back

--- client/src/components/Lobby.vue
+++ client/src/components/Lobby.vue
@@ -9,16 +9,15 @@
         </template>
 
         <v-list>
-          <v-list-item @click="dialogFechaParcial = true; selectErro=false">
+          <v-list-item @click="dialog.FechaParcial = true; selectErro=false">
             <v-list-item-title>Sair do Role</v-list-item-title>
           </v-list-item>
-          <v-list-item @click.stop>   
+          <v-list-item @click="dialog.FechaSala = true">
             <v-list-item-title>Fechar Sala</v-list-item-title>
           </v-list-item>
-          <v-list-item @click="dialogCodSala = true">
+          <v-list-item @click="dialog.CodSala = true">
             <v-list-item-title>Código da Sala</v-list-item-title>
           </v-list-item>
-
         </v-list>
       </v-menu>
     </div>
@@ -33,27 +32,29 @@
     </div>
     <v-container class="c-list-container" fluid>
       <v-list class="c-list">
-        <v-list-item v-for="item in items" :key="item.id" :dense="true" class="c-list-item">
+        <v-list-item v-for="pedido in items" :key="pedido.id" :dense="true" class="c-list-item">
           <v-avatar tile size="20px">
-            <v-img :src="require(`@/assets/Icon/${item.tipo}.png`)"></v-img>
+            <!-- <v-img :src="require(`@/assets/Icon/${item.tipo}.png`)"></v-img> -->
+            <v-img :src="require(`@/assets/Icon/bebida.png`)"></v-img>
+
           </v-avatar>
 
           <v-list-item-content class="c-list-item-content">
             <v-list-item-title
               class="c-item-lista"
-              v-text="item.descricao ? item.descricao : item.tipo"
+              v-text="pedido.quantidade + 'x ' + (pedido.item.nome ? pedido.item.nome : 'pedido.item.tipo')"
             ></v-list-item-title>
             <span class="c-item-lista">
-              <b>{{item.pessoas.length}}</b>
+              <b>{{pedido.perfil.length}}</b>
               <v-icon class="c-icon-item-lista" color="black">people</v-icon>
             </span>
           </v-list-item-content>
 
           <v-list-item-icon style="display:inline-block">
             <v-icon class="c-secundary">monetization_on</v-icon>
-            <span class="c-secundary c-item-preco">{{parseFloat(item.preco).toFixed(2)}}</span>
+            <span class="c-secundary c-item-preco">{{parseFloat(pedido.item.valor * pedido.quantidade).toFixed(2)}}</span>
 
-            <v-btn class="c-nopadding" :ripple="false" icon small text @click="editarItem(item)">
+            <v-btn class="c-nopadding" :ripple="false" icon small text @click="editarItem(pedido)">
               <v-icon class="c-secundary">edit</v-icon>
             </v-btn>
           </v-list-item-icon>
@@ -74,7 +75,7 @@
       </v-btn>
     </v-footer>
 
-    <v-dialog v-model="dialogEdit" persistent max-width="290">
+    <v-dialog v-model="dialog.Edit" persistent max-width="290">
       <v-card>
         <v-card>
           <v-card-title>
@@ -83,10 +84,13 @@
           <v-card-text>
             <v-container>
               <v-row>
-                <v-col cols="12" sm="6" md="4">
+                <v-col cols="8">
                   <v-text-field label="Descrição (Opcional)" v-model="itemSendoEditado.descricao"></v-text-field>
                 </v-col>
-                <v-col cols="12" sm="6">
+                <v-col cols="4">
+                  <v-text-field type="number" label="Qtd" v-model="itemSendoEditado.quantidade"></v-text-field>
+                </v-col>
+                <v-col cols="12">
                   <v-select
                     :items="['bebida', 'comida', 'outros']"
                     v-model="itemSendoEditado.tipo"
@@ -94,7 +98,7 @@
                     required
                   ></v-select>
                 </v-col>
-                <v-col cols="12" sm="6" md="4">
+                <v-col cols="12">
                   <v-text-field
                     :error="precoErro"
                     @change="verificaPreco()"
@@ -103,7 +107,7 @@
                     prefix="$"
                   ></v-text-field>
                 </v-col>
-                <v-col cols="12" sm="6">
+                <v-col cols="12">
                   <v-select
                     :error="selectErro"
                     :items="this.pessoas"
@@ -123,27 +127,29 @@
             </v-container>
           </v-card-text>
           <v-card-actions>
-            <v-btn color="blue darken-1" text @click="dialogExcluir = true; dialogEdit = false">
+            <v-btn color="blue darken-1" text @click="dialog.Excluir = true; dialog.Edit = false">
               <v-icon color="red">delete</v-icon>
             </v-btn>
             <div class="flex-grow-1"></div>
-            <v-btn color="blue darken-1" text @click="dialogEdit = false">Cancelar</v-btn>
+            <v-btn color="blue darken-1" text @click="dialog.Edit = false">Cancelar</v-btn>
             <v-btn
               color="blue darken-1"
               text
-              @click="salvarItem(itemSendoEditado) ? dialogEdit = false:null"
+              @click="salvarItem(itemSendoEditado) ? dialog.Edit = false:null"
             >Salvar</v-btn>
           </v-card-actions>
         </v-card>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="dialogExcluir" persistent max-width="290">
+    <v-dialog v-model="dialog.Excluir" persistent max-width="290">
       <v-card>
         <v-card-title
           class="headline"
         >{{itemSendoEditado.descricao ? itemSendoEditado.descricao : itemSendoEditado.tipo}}</v-card-title>
         <v-card-text>
+          Quantidade:{{itemSendoEditado.quantidade}}
+          <br />
           Preço:{{itemSendoEditado.preco}}
           <br />
           Pagantes: {{itemSendoEditado.pessoas.length}}
@@ -152,15 +158,15 @@
           <v-btn
             color="red darken-1"
             text
-            @click.stop="dialogExcluir = false; excluirItem(itemSendoEditado)"
+            @click.stop="dialog.Excluir = false; excluirItem(itemSendoEditado)"
           >Excluir</v-btn>
           <div class="flex-grow-1"></div>
-          <v-btn color="red darken-1" text @click="dialogExcluir = false">Cancelar</v-btn>
+          <v-btn color="red darken-1" text @click="dialog.Excluir = false">Cancelar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="dialogFechaParcial" persistent max-width="290">
+    <v-dialog v-model="dialog.FechaParcial" persistent max-width="290">
       <v-card>
         <v-card-title>Sair do role</v-card-title>
         <v-card-text>
@@ -168,30 +174,55 @@
           Você não podera retornar ao role.
         </v-card-text>
         <v-card-actions>
-          <v-btn
-            color="red darken-1"
-            text
-            @click.stop="dialogFechaParcial = false; fecharRoleParcial()"
-          >Sair</v-btn>
+          <v-btn color="red darken-1" text @click="fecharRoleParcial()">Sair</v-btn>
           <div class="flex-grow-1"></div>
-          <v-btn color="red darken-1" text @click="dialogFechaParcial = false">Cancelar</v-btn>
+          <v-btn color="red darken-1" text @click="dialog.FechaParcial = false">Cancelar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
-    <v-dialog v-model="dialogCodSala" persistent max-width="290">
+    <v-dialog v-model="dialog.FechaSala" persistent max-width="290">
+      <v-card>
+        <v-card-title>Sair do role</v-card-title>
+        <v-card-text>
+          Deseja Realmente fechar o role?
+          Isso Finalizará o role para todos.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="red darken-1" text @click="fecharRole()">Finalizar</v-btn>
+          <div class="flex-grow-1"></div>
+          <v-btn color="red darken-1" text @click="dialog.FechaSala = false">Cancelar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialog.CodSala" persistent max-width="290">
       <v-card>
         <v-card-title>Código da sala</v-card-title>
         <v-card-text>
-          <h1 style="text-align: center">177013</h1>
+          <h1 style="text-align: center">{{codigoDaSala}}</h1>
         </v-card-text>
         <v-card-actions>
           <div class="flex-grow-1"></div>
-          <v-btn color="black" text @click="dialogCodSala = false">Fechar</v-btn>
+          <v-btn color="black" text @click="dialog.CodSala = false">Fechar</v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
 
+    <v-dialog v-model="dialog.FechaItem" persistent max-width="290">
+      <v-card>
+        <v-card-title>Sua conta é:</v-card-title>
+        <v-card-text>
+          <h1
+            style="text-align: center"
+          >{{(parseFloat(totalPessoal).toFixed(2) + "").replace(".", ",")}}R$</h1>
+        </v-card-text>
+        <v-card-actions>
+          <div class="flex-grow-1"></div>
+          <v-btn color="black" text @click="$router.push('home')">Fechar</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-content>
 </template> 
 
@@ -271,12 +302,12 @@ export default {
       //calcula total do role
       let vm = this;
       vm.totalDoRole = 0;
-      this.items.map(a => (vm.totalDoRole += a.preco));
+      this.items.map(a => (vm.totalDoRole += (a.preco * a.quantidade)));
 
       //calcula parcial do role
       vm.totalPessoal = 0;
       this.items.map(
-        a => (vm.totalPessoal += a.pessoas.includes(vm.idPessoa) ? a.preco : 0)
+        a => (vm.totalPessoal += a.pessoas.includes(vm.idPessoa) ? (a.preco * a.quantidade) : 0)
       );
     },
     novoItem() {
@@ -288,7 +319,7 @@ export default {
         preco: null,
         pessoas: []
       };
-      this.dialogEdit = true;
+      this.dialog.Edit = true;
     },
     salvarItem(item) {
       //valida preco
@@ -328,7 +359,7 @@ export default {
     editarItem(item) {
       this.AcaoItem = "Editar";
       this.itemSendoEditado = { ...item };
-      this.dialogEdit = true;
+      this.dialog.Edit = true;
     },
     verificaPreco() {
       if (this.itemSendoEditado.preco.length <= 0) this.precoErro = true;
@@ -344,11 +375,25 @@ export default {
     },
     fecharRoleParcial() {
       console.log("REQUISIÇÃO POST PARA BACKEND (FECHAR ROLE)");
+      this.dialog.FechaItem = true;
+      this.dialog.FechaParcial = false;
       //no response exibir o total
     },
     opcoesSala() {},
     toggleSelect() {
       this.itemSendoEditado.pessoas = this.pessoas.map(a => a.id);
+    },
+    atualizaJson() {
+       axios
+         .get('http://10.64.41.72:6969/pedidos/salas/1', {auth:{username:'43999032081', password:'admin'}})
+         .then(
+           (response) => {this.items =  response.data},
+           (error) => {console.log(error.data)}
+           )
+         
+
+      console.log("Requisição backend para atualizar os itens");
+      this.recalculaTotal();
     }
   },
   components: {
@@ -358,12 +403,17 @@ export default {
     return {
       totalPessoal: 0,
       totalDoRole: 0,
+      codigoDaSala: "177013",
       precoErro: false,
       selectErro: false,
-      dialogEdit: false,
-      dialogExcluir: false,
-      dialogFechaParcial: false,
-      dialogCodSala: false,
+      dialog: {
+        Edit: false,
+        Excluir: false,
+        FechaParcial: false,
+        FechaSala: false,
+        CodSala: false,
+        FechaItem: false
+      },
       dialogMsg: "",
       AcaoItem: "Editar",
       itemSendoEditado: {
@@ -371,18 +421,19 @@ export default {
         tipo: null,
         descricao: null,
         preco: null,
+        quantidade: null,
         pessoas: []
       },
       items: [...itemsJson],
       pessoas: [...pessoasJson],
-      idPessoa: 1
+      idPessoa: 1,
+      intervaloAtualiza: setInterval(() => {
+        this.atualizaJson();
+      }, 200000)
     };
   },
   mounted() {
-    // this.recalculaTotal();
-    // axios
-    //   .get('http://localhost:6969/pedidos/salas/1')
-    //   .then(response => (this.daniel_pedidos = response.data));
+    this.atualizaJson();
   }
 };
 </script>
