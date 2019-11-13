501169bcfb0752166eaf664ce8283534ee78823d
Vitor <vitim_cristovao@hotmail.com>
2019-11-12 22:42:21 -0200

    Tela de historico integrada com backend

--- client/src/components/Historico.vue
+++ client/src/components/Historico.vue
@@ -2,15 +2,21 @@
 <template>
   <v-content>
     <v-row>
-      <v-col>
+      <v-col class="c-painelHeader">
         <p class="text-center headline">Histórico de Rolês</p>
-        <v-expansion-panels mobile-breakpoint="400" active-class="c-painelAtivo">
-          <t-expansion-panel :key="item.id" v-for="item in items" :historico="item"></t-expansion-panel>
+        <v-expansion-panels mobile-breakpoint="400" active-class="c-painelAtivo" >
+          <t-expansion-panel :key="item.id" v-for="item in historicos" :historico="item"></t-expansion-panel>
         </v-expansion-panels>
       </v-col>
     </v-row>
   </v-content>
 </template>
+
+<style scoped>
+.c-painelHeader{
+  padding: 1px
+}
+</style>
 <script>
 import expansionPanel from "./Templates/expansion-panel";
 import axios from "axios";
@@ -18,7 +24,7 @@ export default {
   data() {
     return {
       historicos: [],
-      idUsuario : 1
+      idUsuario: 2
     };
   },
   components: {
@@ -30,33 +36,27 @@ export default {
     };
     console.log("FAZ REQUISICAO BACKEND PARA POPULAR O HISTORICO");
     console.log("REQJSON: " + JSON.stringify(reqJSON));
-    this.historicos = [
-      {id: 1, data: "23/09/2019", role: "Boteco do Luan", quantidadeParticipantes: 4, valorTotal: 244.67,valorParcial: 58.32},
-      {id: 2, data: "23/09/2019", role: "Dogg's Londrina", quantidadeParticipantes: 2, valorTotal: 224.67,valorParcial: 77.2},
-      {id: 3, data: "23/09/2019", role: "Bar do Ceará", quantidadeParticipantes: 3, valorTotal: 142.67,valorParcial: 36.28},
-      {id: 4, data: "23/09/2019", role: "Garden Chopp", quantidadeParticipantes: 2, valorTotal: 177.67,valorParcial: 48.88},
-      {id: 5, data: "23/09/2019", role: "Delliverya", quantidadeParticipantes: 9, valorTotal: 987.67,valorParcial: 133.76},
-      {id: 6, data: "23/09/2019", role: "New York Louge", quantidadeParticipantes: 5, valorTotal: 344.67,valorParcial: 70.21},
-      {id: 7, data: "23/09/2019", role: "SantArena", quantidadeParticipantes: 3, valorTotal: 198.67,valorParcial: 26.96},
-      {id: 8, data: "23/09/2019", role: "Maximus bar", quantidadeParticipantes: 6, valorTotal: 394.67,valorParcial: 92.13}
-    ];
+    this.historicos = [];
+    this.atualizaJson();
   },
-   atualizaJson() {
+  methods: {
+    atualizaJson() {
       //Carrega itens da sala
       axios
-        .get(`http://54.159.203.154/historicos/usuarios/${this.idUsuario}`, {
+        .get(`http://localhost:6969/historicos/usuarios/${this.idUsuario}`, {
           auth: { username: "43999032081", password: "admin" }
         })
         .then(
           response => {
-            this.items = response.data;
+            console.log(response);
+            this.historicos = response.data;
           },
           error => {
-            console.log(error.data);
+            console.log(error);
           }
         );
       console.log("Requisição backend para atualizar os itens");
-      
+    }
   }
 };
 </script>
--- client/src/components/Templates/expansion-panel.vue
+++ client/src/components/Templates/expansion-panel.vue
@@ -4,24 +4,24 @@
       <div class="col-7 c-painelHeader">
         <span class="title">
           <v-icon color="#9c27b0">where_to_vote</v-icon>
-          {{historico.role}}
+          {{'historico.nomeRole'}}
         </span>
       </div>
-      <div class="col-5 historico">
+      <div>
         <span class="caption c-data">
           <v-icon color="#9c27b0">calendar_today</v-icon>
-          {{ historico.data_saida.day +'/'+ historico.data_saida.month +'/'+ historico.data_saida.year }}
+          {{ historico.data_entrada.substring(8, 10) +'/'+ historico.data_entrada.substring(5, 7) +'/'+ historico.data_entrada.substring(0, 4)}}
         </span>
       </div>
     </v-expansion-panel-header>
     <v-expansion-panel-content>
-      <div class="col-12 d-inline-flex">
-        <div class="col-4 text-center">
+      <div class="col-12 d-inline-flex c-nopadding">
+        <div class="col-4 text-center c-nopadding">
           <p class="text-left font-weight-bold">Rolezeiros:</p>
           <p class="text-left font-weight-bold">Valor Pago:</p>
           <p class="text-left font-weight-bold">Conta:</p>
         </div>
-        <div class="col-4 text-center">
+        <div class="col-4 text-center c-nopadding">
           <p class="text-center font-weight-bold">
             <v-icon color="#9c27b0">people</v-icon>
           </p>
@@ -32,7 +32,7 @@
             <v-icon color="#9c27b0">chrome_reader_mode</v-icon>
           </p>
         </div>
-        <div class="col-4 text-center">
+        <div class="col-4 text-center c-nopadding">
           <p class="text-center font-weight-bold">{{historico.totalUsuarios}}</p> 
           <p class="text-center font-weight-bold">{{historico.totalParcial}}</p>
           <p class="text-center font-weight-bold">{{historico.totalSala}}</p>
@@ -61,6 +61,8 @@ export default {
   padding-left: 0;
   padding-right: 0;
 }
-
+.c-nopadding{
+  padding: 0;
+}
 
 </style>
