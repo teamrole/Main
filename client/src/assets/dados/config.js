0571ee65a1a15a9af6e2f145af49efeb71f1e5e9
Daniel Junior <danjun48@gmail.com>
2019-12-02 02:08:50 -0300

    Merge com CB-corretiva

---
+++ client/src/assets/dados/config.js
@@ -0,0 +1,9 @@
+const config = {
+    api: {
+        url: 'http://54.233.253.133',
+        // auth: { username: "43999999999", password: "123456" }
+        auth: { username: "43999418405", password: "admin" }
+    }
+}
+
+export default config;
---
+++ client/src/components/Templates/panel-historico.vue
@@ -0,0 +1,77 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header @click.stop>
+      <div class="col-7 c-painelHeader">
+        <span class="title">
+          <v-icon color="#9c27b0">where_to_vote</v-icon>
+          {{historico.sala.nome?historico.sala.nome:"Role S/ Nome"}}
+        </span>
+      </div>
+      <div>
+        <span class="caption c-data">
+          <v-icon color="#9c27b0">calendar_today</v-icon>
+          {{ historico.data_entrada.substring(8, 10) +'/'+ historico.data_entrada.substring(5, 7) +'/'+ historico.data_entrada.substring(0, 4)}}
+        </span>
+      </div>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <div class="col-12 d-inline-flex c-nopadding">
+        <div class="col-4 text-center c-nopadding">
+          <p class="text-left font-weight-bold">Rolezeiros:</p>
+          <p class="text-left font-weight-bold">Valor Pago:</p>
+          <p class="text-left font-weight-bold">Conta:</p>
+        </div>
+        <div class="col-4 text-center c-nopadding">
+          <p class="text-center font-weight-bold">
+            <v-icon color="#9c27b0">people</v-icon>
+          </p>
+          <p class="text-center font-weight-bold">
+            <v-icon color="#9c27b0">attach_money</v-icon>
+          </p>
+          <p class="text-center font-weight-bold">
+            <v-icon color="#9c27b0">chrome_reader_mode</v-icon>
+          </p>
+        </div>
+        <div class="col-4 text-center c-nopadding">
+          <p class="text-center font-weight-bold">{{historico.totalUsuarios}}</p>
+          <p
+            class="text-center font-weight-bold"
+          >{{historico.totalParcial.toFixed(2)}} R$</p>
+          <p
+            class="text-center font-weight-bold"
+          >{{historico.totalSala.toFixed(2)}} R$</p>
+        </div>
+      </div>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+
+<script>
+export default {
+  props: {
+    historico: Object
+  },
+  data() {
+    return {
+      idUsuario: 2
+    };
+  }
+};
+</script>
+
+<style scoped>
+.c-secondary {
+  color: #9c27b0;
+}
+.c-painelAtivo > .v-expansion-panel-header,
+.c-painelAtivo > .v-expansion-panel-content {
+  background-color: #eee;
+}
+.c-painelHeader {
+  padding-left: 0;
+  padding-right: 0;
+}
+.c-nopadding {
+  padding: 0;
+}
+</style>
